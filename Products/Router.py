f3ced3ddd36bdd8fe902917a774c01f88e7f81d7
L-9 Transmogrify <transmogrify21@outlook.com>
2025-03-06 15:52:21 +0530

    Updated schema

--- Products/Router.py
+++ Products/Router.py
@@ -12,8 +12,6 @@ from Products.Methods import (
     add_product,
     update_product,
     toggle_product_listing,
-    create_bulk_units,
-    update_product_unit,
     get_product,
     upload_product_images,
     remove_product_images,
