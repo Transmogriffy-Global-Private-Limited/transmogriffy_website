8451e361559e87bdea828c98557e60e1e99fd325
L10Transev <transmogrify17@outlook.com>
2025-03-19 06:18:53 +0000

    methods get cart by user id hasbeen added
    Signed-off-by: L10Transev <transmogrify17@outlook.com>

--- Cart/Router.py
+++ Cart/Router.py
@@ -5,8 +5,8 @@ from fastapi import (
     status,
     HTTPException,
 )
-from .Methods import add_to_cart, increase_quantity, decrease_quantity
-from .Database_Schemas import CartSchema, ManagementQuantity
+from .Methods import add_to_cart, increase_quantity, decrease_quantity,get_cart
+from .Database_Schemas import CartSchema, ManagementQuantity,GetCartOfauser
 
 cart_router = APIRouter()
 
@@ -23,6 +23,18 @@ async def add_to_cart_endpoint(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Failed to add to cart: {str(e)}",
         )
+
+@cart_router.post("/getcartdetails", status_code=status.HTTP_200_OK)
+async def cart_get(management_data:GetCartOfauser):
+    try:
+        result = await get_cart({},management_data)
+        return result
+    except Exception as e:
+         raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to increase quantity: {str(e)}",
+        )
+
 
 # increase the quantity of the product
 @cart_router.post("/increasequantity", status_code=status.HTTP_200_OK)
